df16a7b32bd7241f1be9de5f689d1f9fd6955a56
fekete20 <Antal@DESKTOP>
2020-06-19 15:55:45 +0200

    listing employee works

--- assignment/src/app/list/list.component.ts
+++ assignment/src/app/list/list.component.ts
@@ -1,40 +1,19 @@
-import {Component, NgModule, OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { User } from '../model/user';
-import {Data, Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { AppService } from '../app.service';
-import {MaterialModule} from '../material.module';
-/*
-@NgModule( {
-  imports: [
-    MaterialModule
-  ],
-  exports: [
-    MaterialModule
-  ]
-})
-*/
 
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
- // imports: ['../material.module']
-
 })
-
-
 export class ListComponent implements OnInit {
 
-
-/* exports: [
-    MaterialModule
-  ];
-*/
   Employee: any = [];
   user = new User();
   username: string;
-
 
   constructor(private router: Router,
               private appService: AppService)
